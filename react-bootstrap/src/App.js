16fda2cfc0327f5f5d9b21800dd110bdd9316de9
krzysztofstefanski <krzysztof.stefanski82@gmail.com>
2018-11-17 22:23:09 +0100

    second commit

--- react-bootstrap/src/App.js
+++ react-bootstrap/src/App.js
@@ -7,6 +7,8 @@ class App extends Component {
     return (
       <div>
         <h1>Just Header for today...</h1>
+        <h4>Second header</h4>
+        <p>Some paragraph ...</p>
       </div>
     );
   }
