205a2ccc324453b189dc9fad54758bb8cfb23292
Krzysztof Stefanski <39494955+krzysztofstefanski@users.noreply.github.com>
2018-12-29 23:23:39 +0100

    Update App.js

--- react-bootstrap/src/App.js
+++ react-bootstrap/src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
       <div>
         <h1>Just Header for today...</h1>
       <h2>header2</h2>
+      <h3>header3</h3>
         <h4>Second header...</h4>
         <p>Some paragraph .....</p>
         <h2>header2</h2>
