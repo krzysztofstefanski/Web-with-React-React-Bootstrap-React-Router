4f2fbd816239b9bf18e9a190135b9f4b3d750522
Krzysztof Stefanski <39494955+krzysztofstefanski@users.noreply.github.com>
2019-01-22 23:54:19 +0100

    Update App.js

--- react-bootstrap/src/App.js
+++ react-bootstrap/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Just Header for today...</h1>
+        <h1>Just Header for today....</h1>
       <h2>header2</h2>
       <h3>header3</h3>
         <h4>Second header...</h4>
